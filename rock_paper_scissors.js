4c3d305e871455fa26acea95267a61ea487528ab
Arghya Chakrabarty <arghya2801@gmail.com>
2024-09-02 15:56:34 +0000

    Wrote the playRound() function

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -23,15 +23,34 @@ function getHumanChoice(){
     return human_choice;
 }
 
-// console.log(getComputerChoice());
-// console.log(getHumanChoice());
-
 let human_score = 0;
 let computer_score = 0;
 
 function playRound(human_choice, computer_choice) {
-    let computer_choice = Math.round(getComputerChoice()*10);
-    let human_input = getHumanChoice();
-    
-    
+    if (human_choice == computer_choice){
+        console.log("Both played " + human_choice + ", so tied");
+    } else if (human_choice == 1 && computer_choice == 2){
+        console.log("You lose! " + computer_choice + " beats " + human_choice);
+        computer_score += 1;
+    } else if (human_choice == 1 && computer_choice == 3){
+        console.log("You win! " + human_choice + " beats " + computer_choice);
+        human_score += 1;
+    } else if (human_choice == 2 && computer_choice == 1){
+        console.log("You win! " + human_choice + " beats " + computer_choice);
+        human_score += 1;
+    } else if (human_choice == 2 && computer_choice == 3){
+        console.log("You lose! " + computer_choice + " beats " + human_choice);
+        computer_score += 1;
+    } else if (human_choice == 3 && computer_choice == 1){
+        console.log("You lose! " + computer_choice + " beats " + human_choice);
+        computer_score += 1;
+    } else if (human_choice == 3 && computer_choice == 2){
+        console.log("You win! " + human_choice + " beats " + computer_choice);
+        human_score += 1;
+    }
 }
+
+let computer_choice_function = computerChoiceMove();
+let human_input = getHumanChoice();
+
+console.log(playRound(human_input, computer_choice_function));
